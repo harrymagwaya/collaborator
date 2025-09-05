6052e1976af381cdd27c62756879fbf72dc28e49
mwijuka <mwijukaharoid@gmail.com>
2025-09-05 12:34:38 +0300

    added the cred

--- src/main/java/com/collaborator/collaborator/backend/services/config/SecurityConfig.java
+++ src/main/java/com/collaborator/collaborator/backend/services/config/SecurityConfig.java
@@ -1,15 +1,17 @@
 package com.collaborator.collaborator.backend.services.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.collaborator.collaborator.backend.services.MyUserDetailsService;
@@ -24,6 +26,7 @@ public class SecurityConfig {
 
     @Autowired
     private CustomAuthFailure failureHandler;
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws  Exception{
@@ -45,7 +48,6 @@ public class SecurityConfig {
     }
 
 
-
     @Bean
     public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception{
         return http.getSharedObject(AuthenticationManagerBuilder.class)
@@ -56,8 +58,8 @@ public class SecurityConfig {
     }
 
 
-
-    private PasswordEncoder passwordEncoder() {
+    @Bean
+     BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
     
